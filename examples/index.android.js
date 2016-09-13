158d57fa27dc9e251aed431b13fc9a73c9c0968d
zhangzimeng <zhangzimeng01@baidu.com>
2016-09-13 19:12:57 +0800

    add android

---
+++ examples/index.android.js
@@ -0,0 +1,10 @@
+import {
+    AppRegistry
+} from 'react-native';
+// import App from './src/App'
+
+// import App from './src/ListApp'
+
+import Home from './scene/home'
+
+AppRegistry.registerComponent('ReactNativeAnimation', () => Home);
